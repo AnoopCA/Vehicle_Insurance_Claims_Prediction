05a96017435cdb03b2b3ee447b8aa932b9e89932
Anoop CA <anoopafter@gmail.com>
2024-05-15 04:05:27 +0530

    Featurization updates

--- Read_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv('train_set.csv')
-#print(data.head())
-#print(data.columns)
-#print(data['Household_ID'].head(50))
-print(data['Claim_Amount'].sort_values(ascending=False).head())
